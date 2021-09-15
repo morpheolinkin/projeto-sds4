a7169c09018b2fd62994d2ccb09fda9822416e3c
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2021-09-14 21:12:01 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvenddas/DsvenddasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvenddas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvenddasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvenddasApplication.class, args);
+	}
+
+}
