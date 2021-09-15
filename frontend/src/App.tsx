e8282090b98aa8eee03ef04bbb725b7a1ed80bee
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2021-09-14 21:32:13 -0300

    Project clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <h1>Olá mundo</h1>
     </div>
   );
 }
